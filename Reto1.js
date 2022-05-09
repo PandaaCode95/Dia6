ac6db335265a7d82a738267c22e7d44b73480697
PandaaCode95 <alvaro.amairal@gmail.com>
2022-05-09 18:35:09 +0200

    Subiendo functions

---
+++ Reto1.js
@@ -0,0 +1,22 @@
+let res ;
+function suma (x,y){
+     res = x+ y;
+    return res;
+}
+function rest (x,y){
+     res = x- y;
+    return res;
+}
+function mul (x,y){
+     res = x * y;
+    return res;
+}
+function div (x,y){
+     res = x / y;
+    return res;
+}
+function sqrt(x){
+    res = Math.sqrt(x);
+    return res;
+
+}
